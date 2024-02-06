b2426833cf53ec63b6ac041ccc5ed4130284f60d
Jaxon Adams <jaxon.adams@loanpro.io>
2024-02-06 11:58:24 -0700

    cleaned up parser class and began work on file parsing logic

--- fparser.py
+++ fparser.py
@@ -4,6 +4,11 @@ of a parser include a PyParser and a RubyParser.
 """
 
 
+import re
+
+from icecream import ic # TODO: REMOVE ME
+
+
 class Parser:
     """A base class representing a code parser object."""
 
@@ -13,7 +18,9 @@ class Parser:
     def load_files(self):
         """Lazily load files into memory to be parsed one at a time."""
 
-        # TODO: implement generator to yield file strings one at a time
+        for fname in self.files:
+            with open(fname) as f:
+                yield fname, f.read()
 
         pass
 
@@ -24,7 +31,29 @@ class PyParser(Parser):
     def __init__(self, files):
         super().__init__(files)
 
-        print(self.files)
+        self.data = { fname: {} for fname in self.files }
+        self.load_data()
+
+        print(self.data)
+
+    def load_data(self):
+        """Call member functions to populate the data attribute
+        with code that should be documented.
+        """
+
+        for fname, f_content in self.load_files():
+            # TODO: call functions to populate self.data
+            self.add_docstrings(fname, f_content)
+
+    def add_docstrings(self, fname, f_content):
+        """Read the given file for all docstrings, adding them to self.data"""
+
+        # TODO: FIX ME -- exclude triple-quoted fstrings (f"""somestr""")
+        ds_pattern = r'(\'\'\'(.*?)\'\'\'|\"\"\"(.*?)\"\"\")'
+        ds_matches = re.finditer(ds_pattern, f_content, re.DOTALL)
+        docstrings = [match[1] or match[2] for match in ds_matches]
+
+        self.data[fname]["docstrings"] = docstrings
 
 
 class RubyParser(Parser):
@@ -32,5 +61,3 @@ class RubyParser(Parser):
 
     def __init__(self, files):
         super().__init__(files)
-
-        print(self.files)
