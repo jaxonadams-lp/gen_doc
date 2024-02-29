82fe00e7df7fcb23f60d22cbff5ad97fffe34ba8
Jaxon Adams <jaxon.adams@loanpro.io>
2024-02-29 08:38:11 -0700

    pivoting to markdown generation

--- main.py
+++ main.py
@@ -11,13 +11,12 @@ from pathlib import Path
 
 from directory import Directory
 from fparser import PyParser, RubyParser
-from pdfwriter import PDFWriter
 
 
-def get_path():
-    """Get the path to the project which should be documented."""
+def get_path(message):
+    """Get a file path from the user."""
 
-    print("Please enter the path to the project you'd like documented.")
+    print(message)
     print(f"Current directory: {os.getcwd()}")
     path_in = input("  >> ")
     print("\n")
@@ -25,11 +24,11 @@ def get_path():
     project_path = f"{Path.home()}/{path_in}"
 
     print(f"Path: {path_in}")
-    user_conf = input("Is this the correct path to your project? [Y/n] >> ")
+    user_conf = input("Is this the correct path? [Y/n] >> ")
 
     if user_conf not in ["Y", "y", "yes", ""]:
         print("\n")
-        return get_path()
+        return get_path(message)
 
     return project_path
 
@@ -52,7 +51,7 @@ def main():
 
     print(ascii_title)
 
-    project_path = get_path()
+    project_path = get_path("Please enter the path to the project you'd like documented.")
 
     try:
         os.chdir(project_path)
@@ -70,7 +69,8 @@ def main():
     pyparser = PyParser(directory.files_by_ext(".py"))
     # rbparser = RubyParser(directory.files_by_ext(".rb"))
 
-    writer = PDFWriter(pyparser.pydocs)
+    out_path = get_path("Please enter the full path (including a filename) to"
+                        " which you'd like the documentation exported.")
 
     # for docdata in pyparser.pydocs:
     #     docdata.pprint()
